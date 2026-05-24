beab6a20870802529c842194439ee34697d269f1
Denis Panasyuk <den.dzim@yandex.ru>
2026-05-24 19:58:41 +0300

    Добавлен контроллер и обработчик исключений

--- telemetry/collector/src/main/java/ru/practicum/sht/service/EventService.java
+++ telemetry/collector/src/main/java/ru/practicum/sht/service/EventService.java
@@ -1,4 +1,13 @@
 package ru.practicum.sht.service;
 
+import jakarta.validation.Valid;
+import ru.practicum.sht.model.hub.HubEvent;
+import ru.practicum.sht.model.sensor.SensorEvent;
+
 public class EventService {
+    public void addSensorEvent(@Valid SensorEvent sensorEvent) {
+    }
+
+    public void addHubEvent(@Valid HubEvent hubEvent) {
+    }
 }
