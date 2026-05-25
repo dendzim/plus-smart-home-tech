dd7b9775f2b1f9db68242462abb5df04d21b174b
Denis Panasyuk <den.dzim@yandex.ru>
2026-05-25 17:19:42 +0300

    Добавлены модели для хаба

--- telemetry/collector/src/main/java/ru/practicum/sht/model/hub/DeviceType.java
+++ telemetry/collector/src/main/java/ru/practicum/sht/model/hub/DeviceType.java
@@ -1,7 +1,9 @@
 package ru.practicum.sht.model.hub;
 
-public enum DeviceTypeAvro {
+public enum DeviceType {
     MOTION_SENSOR,
     TEMPERATURE_SENSOR,
-    LIGHT_SENSOR
+    LIGHT_SENSOR,
+    CLIMATE_SENSOR,
+    SWITCH_SENSOR
 }
