0fbf48a6b5c9ab45d98b860a666838758281cf69
Denis Panasyuk <den.dzim@yandex.ru>
2026-05-29 00:17:50 +0300

    исправлена ошибка в клиенте кафки

--- telemetry/collector/src/main/java/ru/practicum/sht/kafka/KafkaAvroProducer.java
+++ telemetry/collector/src/main/java/ru/practicum/sht/kafka/KafkaAvroProducer.java
@@ -4,6 +4,7 @@ import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.common.serialization.VoidSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -28,7 +29,7 @@ public class KafkaAvroProducer {
     private void initProducer() {
         Properties config = new Properties();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, EventAvroSerializer.class);
 
         producer = new KafkaProducer<>(config);
