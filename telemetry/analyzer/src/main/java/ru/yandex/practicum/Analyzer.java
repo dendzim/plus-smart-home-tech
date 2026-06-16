d531d7e53ece599f8eb07b4fdc803314c3f2af8c
Denis Panasyuk <den.dzim@yandex.ru>
2026-06-16 22:12:43 +0300

    лишние импорты убраны

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/Analyzer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/Analyzer.java
@@ -3,9 +3,6 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.yandex.practicum.processor.HubEventProcessor;
-import ru.yandex.practicum.processor.SnapshotProcessor;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
