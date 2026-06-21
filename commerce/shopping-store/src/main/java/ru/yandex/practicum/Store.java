2da90f9bab27c22ab981a4fc9e263e0e883c0604
Denis Panasyuk <den.dzim@yandex.ru>
2026-06-21 20:18:45 +0300

    Добавлены заготовки сервисов

---
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/Store.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum;
+
+public class Store {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/Storage.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum;
+
+public class Storage {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
