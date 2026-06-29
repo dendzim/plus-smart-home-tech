3c7b4d532b52740a9b9b89ee076e76a8570be895
Denis Panasyuk <den.dzim@yandex.ru>
2026-06-29 18:24:27 +0300

    правки конфига

--- commerce/warehouse/src/main/java/ru/yandex/practicum/Storage.java
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/Storage.java
@@ -10,6 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class Storage {
     public static void main(String[] args) {
-        SpringApplication.run(Storage.class);
+        SpringApplication.run(Storage.class, args);
     }
 }
